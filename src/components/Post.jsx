5e2a2ef6f8758e069660579eef11f0fd0b0f25fb
Robson de Jesus <robsondejesus1996@hotmail.com>
2023-01-24 17:22:27 -0300

    implementação dos posts

---
+++ src/components/Post.jsx
@@ -0,0 +1,31 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/robsondejesus1996.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Robson de Jesus</strong>
+            <span>Web developer</span>
+          </div>
+        </div>
+
+        <time title="24 de Maio às 8:13h " dateTime="2023-01-24 08:13:30">
+          Publicado a 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉<a href=""> jane.design/doctorcare</a></p>
+        <p><a href="">#novoprojeto #nlw #rocketseat</a></p>          
+      </div>
+    </article>
+  );
+}
