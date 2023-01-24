baab322d994a77a9773274d44c65efa74eaa7491
Robson de Jesus <robsondejesus1996@hotmail.com>
2023-01-24 15:42:14 -0300

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
-import './global.css'
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
-    <h1>
-      <Header/>
-    </h1>
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Robson de Jesus"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint delectus tenetur debitis eos harum numquam. Architecto cupiditate porro nemo? Sint iusto dolorem ad id eum fuga ullam. Doloribus, veniam labore?
+            "
+          />
+          <Post
+            author="Robson de Jesus"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint delectus tenetur debitis eos harum numquam. Architecto cupiditate porro nemo? Sint iusto dolorem ad id eum fuga ullam. Doloribus, veniam labore?
+            "
+          />
+        </main>
+      </div>
+    </div>
   );
 }
 
