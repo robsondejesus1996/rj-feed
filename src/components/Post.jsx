f50b5fa97e091c69f2df9b1660005480003fb172
Robson de Jesus <robsondejesus1996@hotmail.com>
2023-01-26 15:32:44 -0300

    implementação de estados

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,8 +3,16 @@ import { format, formatDistanceToNow } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR';
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
+import { useState } from "react";
+
+
 
 export function Post({ author, publishedAt, content}) {
+
+  const [comments, setComments] = useState([
+    1,
+    2
+  ])
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR
@@ -14,6 +22,14 @@ export function Post({ author, publishedAt, content}) {
     locale: ptBR,
     addSuffix: true,
   })
+
+  function handleCreateNewComment(){
+    event.preventDefault()
+    
+
+    setComments([...comments, comments.length + 1])
+
+  }
 
   return (
     <article className={styles.post}>
@@ -42,8 +58,11 @@ export function Post({ author, publishedAt, content}) {
           })
         }
       </div>
-      <form className={styles.commentForm}>
+
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixei seu feedback</strong>
+
+
         <textarea placeholder="Deixe um comentário" />
 
         <footer>
@@ -52,9 +71,11 @@ export function Post({ author, publishedAt, content}) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment =>{
+          return <Comment
+            key={comment}
+          />
+        })}
       </div>
     </article>
   );
